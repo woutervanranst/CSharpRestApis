564750092f211aa84ce62e7a4b0b66da623082ed
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-01-09 21:53:10 +0100

    aspnetcoreless function

--- HttpTriggerFunction/Program.cs
+++ HttpTriggerFunction/Program.cs
@@ -1,14 +1,31 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+namespace Company.Function
+{
+    public class HttpExample
     {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-    })
-    .Build();
+        private readonly ILogger<HttpExample> _logger;
 
-host.Run();
+        public HttpExample(ILogger<HttpExample> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("HttpExample")]
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("HttpExample");
+            logger.LogInformation("message logged");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString("Welcome to .NET isolated worker !!");
+
+            return response;
+        }
+    }
+}
