f75796a360f60b56dc253ed577c91d0804f77065
Wouter Van Ranst <wouter.vanranst@gmail.com>
2025-01-09 22:08:11 +0100

    feat: correct net8 isolated

--- HttpTriggerFunction/Program.cs
+++ HttpTriggerFunction/Program.cs
@@ -1,31 +1,41 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Net;
 
-namespace Company.Function
+var builder = FunctionsApplication.CreateBuilder(args);
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+// builder.Services
+//     .AddApplicationInsightsTelemetryWorkerService()
+//     .ConfigureFunctionsApplicationInsights();
+
+builder.Build().Run();
+
+
+
+public class HttpExample
 {
-    public class HttpExample
+    private readonly ILogger<HttpExample> _logger;
+
+    public HttpExample(ILogger<HttpExample> logger)
     {
-        private readonly ILogger<HttpExample> _logger;
+        _logger = logger;
+    }
 
-        public HttpExample(ILogger<HttpExample> logger)
-        {
-            _logger = logger;
-        }
+    [Function("HttpExample")]
+    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        var logger = executionContext.GetLogger("HttpExample");
+        logger.LogInformation("message logged");
 
-        [Function("HttpExample")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req,
-            FunctionContext executionContext)
-        {
-            var logger = executionContext.GetLogger("HttpExample");
-            logger.LogInformation("message logged");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        response.WriteString("Welcome to .NET isolated worker !!");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-            response.WriteString("Welcome to .NET isolated worker !!");
-
-            return response;
-        }
+        return response;
     }
 }
